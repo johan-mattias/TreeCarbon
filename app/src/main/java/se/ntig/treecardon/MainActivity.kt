1ffa153ea6c3b89825aabb18d075c515617de19d
Johan Snider <johan.snider@ntig.se>
2020-12-15 16:51:01 +0100

    now the data for one tree can be saved

--- app/src/main/java/se/ntig/treecardon/MainActivity.kt
+++ app/src/main/java/se/ntig/treecardon/MainActivity.kt
@@ -1,15 +1,24 @@
 package se.ntig.treecardon
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject.NULL
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sharedPref = getPreferences(Context.MODE_PRIVATE)
+        sharedPref?.let {
+            val vs_from_shared = sharedPref.getFloat("ek", 0.0.toFloat())
+            editTextResult.setText(vs_from_shared.toString())
+        }
+
     }
 
     fun calculate(view: View) {
@@ -21,14 +30,20 @@ class MainActivity : AppCompatActivity() {
 
         when (tree) {
             "ek" -> {
-                var temp1 = 0.03522*(diameter*diameter)*height
+                var temp1 = (0.03522*diameter)*(0.03522*diameter)*height
                 var temp2 = 0.08772*diameter*height
-                var temp3 = 0.04905*(diameter*diameter)
+                var temp3 = (0.04905*diameter)*(0.04905*diameter)
                 VS = ((temp1 + temp2) - temp3)
             }
         }
 
         editTextResult.setText(VS.toString())
+
+        val sharedPref = getPreferences(Context.MODE_PRIVATE) ?: return
+        with (sharedPref.edit()) {
+            putFloat(tree.toString(), VS.toFloat())
+            apply()
+        }
     }
 
 
